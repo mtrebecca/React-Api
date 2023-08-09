90eaadf55697fd41352a30e7151f6ee2bd67c5a6
mrebecca <rebecca@peoplexperience.com>
2023-08-09 15:09:55 -0300

    form restaurant add

---
+++ src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
@@ -0,0 +1,32 @@
+import { Button, TextField } from "@mui/material"
+import { useState } from 'react';
+import axios from 'axios';
+
+const FormularioRestaurante = () => {
+
+    const [nomeRestaurante, setNomeRestaurante] = useState('')
+
+    const aoSubmeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
+        evento.preventDefault()
+
+        axios.post('http://localhost:8000/api/v2/restaurantes/', {
+            nome: nomeRestaurante
+        })
+            .then(() => {
+                alert("Restaurante cadastrado com sucesso!")
+            })
+    }
+
+    return (
+        <form onSubmit={aoSubmeterForm}>
+            <TextField value={nomeRestaurante} 
+            onChange={evento => setNomeRestaurante(evento.target.value)} 
+            id="standard-basic" 
+            label="Nome do Restaurante" 
+            variant="standard" />
+            <Button type="submit" variant="outlined">Salvar</Button>
+        </form>
+    )
+}
+
+export default FormularioRestaurante
